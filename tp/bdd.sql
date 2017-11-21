29f811c97ad4ea8bf5db3b83ca46333c59ceb434
Anthony <anthony_bouillon@outlook.fr>
2017-11-21 08:39:41 +0100

    TP créer une base de donnée ainsi qu'une table et des colonnes

---
+++ tp/bdd.sql
@@ -0,0 +1,4 @@
+TP
+Créer la base codex. Y créer une table clients qui aura comme colonnes :
+CREATE DATABASE codex;
+CREATE TABLE clients (id INT AUTO_INCREMENT, lastName VARCHAR(20), firstName VARCHAR(20), birthDate DATE, adress VARCHAR(20), firstPhoneNumber INT, secondPhoneNumber INT, mail VARCHAR(20), PRIMARY KEY (id));
